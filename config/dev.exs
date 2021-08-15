097c2be17aff2d1b2aef3ca31cf5a2bbd4a8a39f
Sergio Tapia <yeyo@hey.com>
2021-08-14 20:54:19 -0400

    Configure CSS builds using postcss

--- config/dev.exs
+++ config/dev.exs
@@ -23,7 +23,15 @@ config :golden, GoldenWeb.Endpoint,
   check_origin: false,
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
