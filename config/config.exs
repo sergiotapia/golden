8c87657b83c81c7c5e123a99396503800ea74f37
Sergio Tapia <yeyo@hey.com>
2021-11-10 12:43:59 -0500

    Fix Alpinejs 3.5.0 by changing target to es2017

--- config/config.exs
+++ config/config.exs
@@ -22,7 +22,7 @@ config :golden, GoldenWeb.Endpoint,
 config :esbuild,
   version: "0.12.17",
   default: [
-    args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
+    args: ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
